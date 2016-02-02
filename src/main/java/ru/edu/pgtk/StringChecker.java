a8f7565d6583bf85ac1126da4a8d957ffdd5dc4a
Voronin Leonid <voronin.leonid@pgtk.edu.ru>
2016-02-02 09:31:54 +0700

    alternative reality

--- src/main/java/ru/edu/pgtk/StringChecker.java
+++ src/main/java/ru/edu/pgtk/StringChecker.java
@@ -2,23 +2,10 @@ package ru.edu.pgtk;
 
 public class StringChecker {
 
-    /**
-     * Проверяет строку на пустоту. Допускается null в качестве параметра.
-     *
-     * @param source Строка для проверки. Допускается null
-     * @return Истина, если строка равна null или пустая. Иначе - ложь.
-     */
     static public boolean isEmpty(String source) {
         return source == null || source.isEmpty();
     }
 
-    /**
-     * Проверяет строку на пустоту. Допускается null в качестве параметра.
-     *
-     * @param source Строка для проверки. Допускается null
-     * @return Истина, если строка равна null, пустая или содержит пробельные символы.
-     * Иначе - ложь.
-     */
     public static boolean isBlank(String source) {
         return source == null || source.trim().isEmpty();
     }
